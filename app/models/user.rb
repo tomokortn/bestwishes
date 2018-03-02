6c542d86601d6a3c3224dc2c42742ab1a196bd40
tomoko <tomokortn@gmail.com>
2018-03-02 21:37:44 +0900

    順番をかえた

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :posts
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-	devise :rememberable, :trackable, :omniauthable, omniauth_providers: [:facebook]
 
   def email_required?
     false
   end
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+	devise :rememberable, :trackable, :omniauthable, omniauth_providers: [:facebook]
+
 
 end
