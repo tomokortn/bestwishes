5a2e74d3c0abfa255594a2087d68ae985ba269f3
tomoko <tomokortn@gmail.com>
2018-03-02 17:46:08 +0900

    コントローラ変えた

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  before_filter :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :move_to_login, except: :login_url
 
